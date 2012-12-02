b3b5eaa6959f7e89c7c17d24aa032eb036ae5423
Anton Chebotaev <anton.chebotaev@gmail.com>
2012-12-02 20:01:19 +0400

    Default settings is set to local postgres database. If host is not production (nest-frontend for now), fallback is set to sqlite

--- website/andreev_ru/settings.py
+++ website/andreev_ru/settings.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import os
+import socket
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -13,14 +14,24 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'local-database',               # Or path to database file if using sqlite3.
-        'USER': '',                             # Not used with sqlite3.
-        'PASSWORD': '',                         # Not used with sqlite3.
-        'HOST': '',                             # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                             # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'andreev_ru',                               # Or path to database file if using sqlite3.
+        'USER': 'postgres',                                 # Not used with sqlite3.
+        'PASSWORD': 'password',                             # Not used with sqlite3.
+        'HOST': 'localhost',                                # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                                         # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+if 'nest-frontend' != socket.gethostname():
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'local-database',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
 
 TIME_ZONE = 'Europe/Moscow'
 LANGUAGE_CODE = 'ru-ru'
