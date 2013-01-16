0146af69ec6f672114920e4902e0a3589ef77cb0
Anton Chebotaev <anton.chebotaev@gmail.com>
2013-01-17 00:27:19 +0400

    closes #21
    Now http://localhost:8000/ru/team/#person_name works

--- website/andreev_ru/main/templatetags/custom_tags.py
+++ website/andreev_ru/main/templatetags/custom_tags.py
@@ -1,8 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import re
+import trans
+
 from django import template
+from django.template.defaultfilters import stringfilter
 from django.core.exceptions import ObjectDoesNotExist
 from andreev_ru.main.models import CustomString
 
 register = template.Library()
+
+PUNCT_RE = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
+@register.filter(is_safe=True)
+@stringfilter
+def transliterate(value, delim=u'-'):
+    return value.lower().encode('trans/slug')
 
 @register.tag(name="custom_string")
 def custom_string(parser, token):
